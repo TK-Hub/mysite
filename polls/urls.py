5d332dbd2cff21f7db429c9902b114b67306943b
TK-Hub <54644447+TK-Hub@users.noreply.github.com>
2020-04-12 21:30:30 +0200

    Chapter 3

--- polls/urls.py
+++ polls/urls.py
@@ -5,5 +5,14 @@ from . import views
 # It goes to views and runs the index function
 # This happens after you came through the mother urls-file in mysite already.
 urlpatterns = [
+    # ex: /polls/
     path('', views.index, name='index'),
+    # ex: /polls/5/ (Five is just a generic id here)
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
+
+# vote, results etc. are the views
