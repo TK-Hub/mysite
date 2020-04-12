c5a52d9dcdd0337d299cf23c73942830971e5b5e
TK-Hub <54644447+TK-Hub@users.noreply.github.com>
2020-04-12 23:25:13 +0200

    Part. 3

--- polls/urls.py
+++ polls/urls.py
@@ -4,6 +4,7 @@ from . import views
 # This determines where you go when you just type in the basic URL (hence, the '')
 # It goes to views and runs the index function
 # This happens after you came through the mother urls-file in mysite already.
+app_name = 'polls'
 urlpatterns = [
     # ex: /polls/
     path('', views.index, name='index'),
